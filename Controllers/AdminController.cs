3ce6a63bb65de114bef3dc7417d0c91ea8c1e20b
kaantamerurkun <urkun33@gmail.com>
2025-04-15 15:23:45 +0300

    Initial commit

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimeMarket.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult AdminLogin()
+        {
+            return View();
+        }
+        public IActionResult AdminDashboard()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimeMarket.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+        public IActionResult User_Listing_Details()
+        {
+            return View();
+        }
+
+        public IActionResult User_MainPage()
+        {
+            return View();
+        }
+        public IActionResult MyProfilePage()
+        {
+            return View();
+        }
+        public IActionResult MyBookmarks()
+        {
+            return View();
+        }
+        public IActionResult EditProfile()
+        {
+            return View();
+        }
+        public IActionResult LiveChat()
+        {
+            return View();
+        }
+        public IActionResult OtherUserProfile()
+        {
+            return View();
+        }
+        public IActionResult CreateListing()
+        {
+            return View();
+        }
+        public IActionResult MyShoppingCart()
+        {
+            return View();
+        }
+        public IActionResult PaymentPage()
+        {
+            return View();
+        }
+        public IActionResult MyProfitLossReport()
+        {
+            return View();
+        }
+        public IActionResult EmailVerification()
+        {
+            return View();
+        }
+        public IActionResult AllMessages()
+        {
+            return View();
+        }
+        public IActionResult AllNotifications()
+        {
+            return View();
+        }
+        public IActionResult MyListing()
+        {
+            return View();
+        }
+    }
+}
